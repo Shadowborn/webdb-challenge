82875b9c4a62366461b5cc85c8261dc8ac34bf2f
Shadowborn <james.starks023@gmail.com>
2019-06-07 09:39:16 -0600

    action routes set up. Now just need to join the tables

--- index.js
+++ index.js
@@ -56,6 +56,50 @@ server.post('/api/projects', async (req, res) => {
 
 
 
+
+
+// list all actions
+server.get('/api/actions', async (req, res) => {
+    // get the actions from the database
+    try {
+      const actions = await db('actions'); // all the records from the table
+      res.status(200).json(actions);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  // list a role by id
+  server.get('/api/actions/:id', async (req, res) => {
+    // get the actions from the database
+    try {
+      const role = await db('actions')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(role);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
+  // create actions
+  server.post('/api/actions', async (req, res) => {
+    try {
+      const [id] = await db('actions').insert(req.body);
+  
+      const role = await db('actions')
+        .where({ id })
+        .first();
+  
+      res.status(201).json(role);
+    } catch (error) {
+      const message = errors[error.errno] || 'We ran into an error';
+      res.status(500).json({ message, error });
+    }
+  });
+
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
